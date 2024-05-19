5dbdfb3a1dd5d39b95ebe40be36d52f0413630e5
Kevin Mozzillo <kevinmozzillo@gmail.com>
2024-05-19 13:32:24 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const counter = document.querySelector(".counter-number");
+async function updateCounter() {
+    let response = await fetch("https://ab4b44z7oojawmyv57sdltgxd40piwdq.lambda-url.us-east-1.on.aws/");
+    let data = await response.json();
+    counter.innerHTML = ' Views: ${data}';
+}
+
+updateCounter()
