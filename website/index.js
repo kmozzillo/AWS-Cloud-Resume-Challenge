00361a1e7e58d1914596b14bbf54236d678ae318
Kevin Mozzillo <kevinmozzillo@gmail.com>
2024-06-10 19:04:53 -0400

    Fixed view counter in javascript file

--- website/index.js
+++ website/index.js
@@ -2,7 +2,7 @@ const counter = document.querySelector(".counter-number");
 async function updateCounter() {
     let response = await fetch("https://ab4b44z7oojawmyv57sdltgxd40piwdq.lambda-url.us-east-1.on.aws/");
     let data = await response.json();
-    counter.innerHTML = ' Views: ${data}';
+    counter.innerHTML = `Views: ${data}`;
 }
 
 updateCounter()
